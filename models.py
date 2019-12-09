50b104a4cb677a2505f7a714f0976cc0b796adf6
John Mantus <jmantus@u.rochester.edu>
2019-12-08 20:30:16 -0500

    changed db import

--- models.py
+++ models.py
@@ -1,7 +1,7 @@
 from flask_login import UserMixin
 from itsdangerous import TimedJSONWebSignatureSerializer as Serializer
 from flask import current_app
-# import db
+from __init__ import db
 
 class User(UserMixin, db.Model):
     id = db.Column(db.Integer, primary_key=True)
