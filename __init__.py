744a82c9c4d5da5de208bf7bc3d7f9fc3baf4ee7
xiaojun-min <55560683+xiaojun-min@users.noreply.github.com>
2019-12-09 18:59:14 -0500

    Update __init__.py
    add email confirmation

--- __init__.py
+++ __init__.py
@@ -1,15 +1,16 @@
 from flask import Flask
+from flask_mail import Mail
 from flask_sqlalchemy import SQLAlchemy
 from flask_login import LoginManager
 
-# Example
-
+mail = Mail()
 db = SQLAlchemy()
 
 app = Flask(__name__)
 
 app.config['SECRET_KEY'] = 'thisismysecretkeydonotstealit'
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///db.sqlite3'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 db.init_app(app)
 
