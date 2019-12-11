19c59309b40cf2d2cecf24b53e4a87517f2b7a87
xiaojun-min <55560683+xiaojun-min@users.noreply.github.com>
2019-12-11 00:35:14 -0500

    Update models.py

--- models.py
+++ models.py
@@ -29,6 +29,11 @@ class User(UserMixin, db.Model):
     def generate_confirmation_token(self, expiration=3600):
         s = Serializer(current_app.config['SECRET_KEY'], expiration)
         return s.dumps({'confirm': self.id}).decode('utf-8')
+    
+    def generate_reset_token(self, expiration=3600):
+        s = Serializer(current_app.config['SECRET_KEY'], expiration)
+        return s.dumps({'reset': self.id}).decode('utf-8')   
+
 
     def confirm(self, token):
         s = Serializer(current_app.config['SECRET_KEY'])
@@ -42,6 +47,23 @@ class User(UserMixin, db.Model):
         db.session.add(self)
         db.session.commit()
         return True
+    
+    @staticmethod
+    def reset_password(token, new_password):
+        s = Serializer(current_app.config['SECRET_KEY'])
+        try:
+            data = s.loads(token.encode('utf-8'))
+        except:
+            return False
+        user = User.query.get(data.get('reset'))
+        if user is None:
+            return False
+        user.password = new_password
+        user.password = generate_password_hash(user.password, method='sha256')  
+        db.session.add(user)
+        return True
+    
+    
 
 class Event(db.Model):
     id = db.Column(db.Integer, primary_key=True, autoincrement=True)
